7cf1776bea965871c27c7e56f75d49260840fa7e
zyunzou.yagi <zyunzou.yagi@gmail.com>
2020-07-18 04:59:35 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
